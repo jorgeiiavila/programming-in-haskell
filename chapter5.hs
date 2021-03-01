bc0f4bfd3c6d7bc0bd325925f337a23759b9f747
Jorge Iribe <jorgeiribe03@gmail.com>
2021-02-28 22:50:33 -0600

    ✨ Solve chapter 5 exercises

--- chapter5.hs
+++ chapter5.hs
@@ -1,6 +1,50 @@
 import Data.Char
 
--- Caesar Cipher
+-- Chapter 5 - List Comprehension
+
+find :: Eq a => a -> [(a,b)] -> [b]
+find k t = [v | (k',v) <- t, k == k']
+
+-- Exercises
+
+-- Problem 1
+squaresSum :: Int
+squaresSum = sum [x * x | x <- [1..100]]
+
+-- Problem 2
+grid :: Int -> Int -> [(Int, Int)]
+grid m n = [(x, y) | x <- [0..m], y <- [0..n]]
+
+-- Problem 3
+square n = [(x, y) | (x, y) <- grid n n, x /= y]
+
+-- Problem 4
+replicate' :: Int -> a -> [a]
+replicate' n x = [x | _ <- [1..n]]
+
+-- Problem 5
+pyths :: Int -> [(Int, Int, Int)]
+pyths n = [(x, y, z) | x <- [1..n], y <- [1..n], z <- [1..n], x^2 + y^2 == z^2]
+
+-- Problem 6
+factors :: Int -> [Int]
+factors n = [x | x <- [1..n], n `mod` x == 0]
+
+perfects :: Int -> [Int]
+perfects n = [x | x <- [2..n], sum (factors x) - x == x]
+
+-- Problem 7
+problemSeven = concat [[(x,y) | y <- [3,4]] | x <- [1,2]]
+
+-- Problem 8
+positions' :: Eq a => a -> [a] -> [Int]
+positions' x xs = [i | i <- find x (zip xs [0..])]
+
+-- Problem 9
+scalarproduct :: [Int] -> [Int] -> Int
+scalarproduct xs ys = sum [x * y | (x, y) <- zip xs ys]
+
+-- Problem 10 - Caesar Cipher
 let2Int :: Char -> Int
 let2Int c = ord c - ord 'a'
 
