2c7c11dee0495e1c2e69520e4f999aa811e8e0d2
Jorge Iribe <jorgeiribe03@gmail.com>
2021-10-20 22:33:32 -0500

    refactor: refactor exercises 2 and 3

--- chapter12/ex2.hs
+++ chapter12/ex2.hs
@@ -1,3 +1,5 @@
-instance Functor ((->) a) where
-  -- fmap :: (b -> c) -> (a -> b) -> (a -> c)
-  fmap = (.)
+newtype F a b = F (a -> b)
+
+instance Functor (F a) where
+  -- fmap :: (a -> b) -> F (a1 -> a) -> F (a1 -> b)
+  fmap f (F x) = F (f . x)
--- chapter12/ex3.hs
+++ chapter12/ex3.hs
@@ -1,6 +1,20 @@
-instance Applicative ((->) r) where
-  -- pure :: a -> r -> a
-  pure = const
+newtype F a b = F (a -> b)
 
-  -- <*> :: (r -> a -> b) -> (r -> a) -> (r -> b)
-  (<*>) f g x = f x (g x)
+instance Functor (F z) where
+  -- fmap :: (a -> b) -> F (z -> a) -> F (z -> b)
+  fmap f (F x) = F (f . x)
+
+instance Applicative (F z) where
+  -- pure :: a -> F z a
+  pure x = F (const x)
+
+  -- <*> :: F z (a -> b) -> F z a -> F z b
+  (<*>) (F f) (F g) = F (\x -> f x (g x))
+
+(F x) = pure (+ 10)
+
+(F y) = (+ 10) <$> F (+ 100)
+
+main = do
+  print (x 20 30)
+  print (y 50)
