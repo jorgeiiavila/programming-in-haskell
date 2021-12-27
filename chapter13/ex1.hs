9f42647820d24ed41a1b75dcecd80eb78659910d
Jorge Iribe <jorgeiribe03@gmail.com>
2021-12-26 22:05:55 -0700

    feat: solve chapter 13 exercise 1

---
+++ chapter13/ex1.hs
@@ -0,0 +1,97 @@
+{-# OPTIONS_GHC -Wno-incomplete-patterns #-}
+
+import Control.Applicative
+import Data.Char
+
+newtype Parser a = P (String -> [(a, String)])
+
+parse :: Parser a -> String -> [(a, String)]
+parse (P p) = p
+
+item :: Parser Char
+item =
+  P
+    ( \inp -> case inp of
+        [] -> []
+        (x : xs) -> [(x, xs)]
+    )
+
+instance Functor Parser where
+  -- fmap :: (a -> b) -> Parser a -> Parser b
+  fmap g p =
+    P
+      ( \inp -> case parse p inp of
+          [] -> []
+          [(v, out)] -> [(g v, out)]
+      )
+
+instance Applicative Parser where
+  -- pure :: a -> Parser a
+  pure v = P (\inp -> [(v, inp)])
+
+  -- (<*>) :: Parser (a -> b) -> Parser a -> Parser b
+  pg <*> px =
+    P
+      ( \inp -> case parse pg inp of
+          [] -> []
+          [(g, out)] -> parse (fmap g px) out
+      )
+
+instance Monad Parser where
+  -- (>>=) :: Parser a -> (a -> Parser b) -> Parser b
+  p >>= f =
+    P
+      ( \inp -> case parse p inp of
+          [] -> []
+          [(v, out)] -> parse (f v) out
+      )
+
+instance Alternative Parser where
+  -- empty :: Parser a
+  empty = P (\inp -> [])
+
+  -- (<|>) :: Parser a -> Parser a -> Parser a
+  p <|> q =
+    P
+      ( \inp -> case parse p inp of
+          [] -> parse q inp
+          [(v, out)] -> [(v, out)]
+      )
+
+sat :: (Char -> Bool) -> Parser Char
+sat p = do
+  x <- item
+  if p x then return x else empty
+
+char :: Char -> Parser Char
+char x = sat (== x)
+
+string :: String -> Parser String
+string [] = return []
+string (x : xs) = do
+  char x
+  string xs
+  return (x : xs)
+
+space :: Parser ()
+space =
+  do
+    many (sat isSpace)
+    return ()
+
+token :: Parser a -> Parser a
+token p = do
+  space
+  v <- p
+  space
+  return v
+
+symbol :: String -> Parser String
+symbol xs = token (string xs)
+
+-- Exercise 1
+comment :: Parser ()
+comment = do
+  symbol "--"
+  many (sat (/= '\n'))
+  return ()
